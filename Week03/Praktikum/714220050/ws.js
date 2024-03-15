89667a4f8cf8b5f829f106b0f490bd7bfeccfe03
mhrndiva <ariadiva00@gmail.com>
2024-03-15 15:28:20 +0700

    praktikumweek03

---
+++ Week03/Praktikum/714220050/ws.js
@@ -0,0 +1,37 @@
+function PostSignUp(namadepan,namabelakang,email,password){
+    var myHeaders = new Headers();
+    myHeaders.append("Login", "chapter03");
+    myHeaders.append("Content-Type", "application/json");
+   
+    var raw = JSON.stringify({
+      "namadepan": namadepan,
+      "namabelakang": namabelakang,
+      "email": email,
+      "password": password
+    });
+   
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+   
+    fetch("https://eor1wiq10obvy4n.m.pipedream.net", requestOptions)
+      .then(response => response.text())
+      .then(result => GetResponse(result))
+      .catch(error => console.log('error', error));
+
+
+    }
+    function PushButton(){
+        namadepan=document.getElementById("namadepan").value;
+        namabelakang=document.getElementById("namabelakang").value;
+        email=document.getElementById("email").value;
+        password=document.getElementById("password").value;
+        PostSignUp(namadepan,namabelakang,email,password);
+  }
+
+  function GetResponse(result){
+    document.getElementById("formsignup").innerHTML = result
+}
