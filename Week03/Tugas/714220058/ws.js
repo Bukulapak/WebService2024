425ef8300e85b588677974b7ee52d76d2dcf9303
qintharganteng <qintharalmaliki@gmail.com>
2024-03-21 20:48:36 +0700

    3-2B-714220058-Muhammad Qinthar

---
+++ Week03/Tugas/714220058/ws.js
@@ -0,0 +1,36 @@
+function PostSignUp(namadepan, namabelakang, email, password, number, belimenu) {
+    var myHeaders = new Headers();
+    myHeaders.append("Login", "chapter03");
+    myHeaders.append("Content-Type", "application/json");
+   
+    var raw = JSON.stringify({
+        "namadepan": namadepan,
+        "namabelakang": namabelakang,
+        "email": email,
+        "password": password,
+        "number": number,
+        "belimenu": belimenu    
+    });
+   
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+   
+    fetch("https://eopz56f1hs28mrm.m.pipedream.net", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
+
+function PushButton() {
+     namadepan = document.getElementById("namadepan").value;
+     namabelakang = document.getElementById("namabelakang").value;
+     email = document.getElementById("email").value;
+     password = document.getElementById("password").value;
+     number = document.getElementById("number").value;
+     belimenu = document.getElementById("belimenu").value;
+    PostSignUp(namadepan, namabelakang, email, password, number, belimenu);
+}
