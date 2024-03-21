f222f73481bea91af73a2e774a47a32a5714a34f
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-03-21 19:23:26 +0700

    3-2B-714220042-Ruth Diana Purnamasari Sagala

--- Week03/Tugas/714220042/ws.js
+++ Week03/Tugas/714220042/ws.js
@@ -1,44 +1,43 @@
-function PostSignUp(namalengkap,namadepan,tempattgllahir,alamat,notelphp,alamatorangtuawali,emailaddress,noTelpHP){
-    var myHeaders = new Headers();
-    myHeaders.append("Login", "chapter03");
-    myHeaders.append("Content-Type", "application/json");
-   
-    var raw = JSON.stringify({
-      "namalengkap": namalengkap,
-      "namapanggilan": namapanggilan,
-      "tempattgllahir": tempattgllahir,
-      "alamat": alamat,
-      "notelphp": notelphp,
-      "alamatorangtuawali": alamatorangtuawali,
-      "emailaddress": emailaddress,
-      "noTelpHP": noTelpHP
-    });
-   
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-   
-    fetch("https://eobh0ts7b6p4pfu.m.pipedream.net", requestOptions)
-      .then(response => response.text())
-      .then(result => GetResponse.log(result))
-      .catch(error => console.log('error', error));
-    }
+function PostSignUp(namadepan,namabelakang,email,password){
+  var myHeaders = new Headers();
+  myHeaders.append("Login", "chapter03");
+  myHeaders.append("Content-Type", "application/json");
+ 
+  var raw = JSON.stringify({
+    "namadepan": namadepan,
+    "namabelakang": namabelakang,
+    "notlp": notlp,
+    "alamat": alamat,
+    "jeniskelas": jeniskelas,
+    //"tglPendaftaran": tglPendaftaran
+  });
+ 
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+ 
+  fetch("https://eoye0d3l4gh6rfj.m.pipedream.net", requestOptions)
+    .then(response => response.text())
+    .then(result => GetResponse(result))
+    .catch(error => console.log('error', error));
+  }
 
-    function GetResponse(result){
-        document.getElementById("formsignup").innerHTML = result;
+  function GetResponse(result){
+      document.getElementById("formsignup").innerHTML = result;
   }
 
   function PushButton(){
-    namalengkap=document.getElementById("namalengkap").value;
-    namapanggilan=document.getElementById("namapanggilan").value;
-    tempattgllahir=document.getElementById("tempattgllahir").value;
-    alamat=document.getElementById("alamat").value;
-    notelphp=document.getElementById("notelphp").value;
-    alamatorangtuawali=document.getElementById("alamatorangtuawali").value;
-    emailaddress=document.getElementById("emailaddresst").value;
-    noTelpHP=document.getElementById("noTelpHP").value;
-    PostSignUp(namalengkap,namapanggilan,tempattgllahir,alamat,notelphp,alamatorangtuawali,emailaddress,noTelpHP);
-}
+      namadepan=document.getElementById("namadepan").value;
+      namabelakang=document.getElementById("namabelakang").value;
+      notlp=document.getElementById("notlp").value;
+      alamat=document.getElementById("alamat").value;
+      booktitle=document.getElementById("jeniskelas").value;
+      //tglpinjam=document.getElementById("tglPendaftaran").value;
+      PostSignUp(namadepan,namabelakang,notlp,alamat,jeniskelas);
+      document.getElementById("formsignup").style.display = 'none';
+      document.getElementById("formsignup").style.display = 'block';
+  }
+
